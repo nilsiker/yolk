669556ba985a86b2074cb61a68387fda491d6e14
Andreas Nilsson <nilsiker94@gmail.com>
2025-07-05 17:20:50 +0200

    wip

--- Yolk.ExampleGame/debug_panel/DebugPanel.cs
+++ Yolk.ExampleGame/debug_panel/DebugPanel.cs
@@ -1,10 +1,19 @@
 namespace Yolk;
 
+using Chickensoft.AutoInject;
+using Chickensoft.Introspection;
 using Godot;
 
+[Meta(typeof(IAutoNode))]
 public partial class DebugPanel : PanelContainer {
+  public override void _Notification(int what) => this.Notify(what);
+
+  [Node] private Label FPSValue { get; set; } = default!;
 
   public override void _Ready() => Visible = false;
+
+  public override void _Process(double delta) => FPSValue.Text = Engine.GetFramesPerSecond().ToString();
+
 
   public override void _Input(InputEvent @event) {
     if (@event.IsActionPressed(Inputs.ToggleDebug)) {
